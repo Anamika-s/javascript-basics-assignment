7efe7b9f8ed1461e8f9d6527c669035ee0884878
Anamika.Sawhney <anamika.9.sawhney@niit.com>
2021-08-23 00:20:17 +0530

    Update q3_flatten_n_dimensional_array.js

--- solutions/q3_flatten_n_dimensional_array.js
+++ solutions/q3_flatten_n_dimensional_array.js
@@ -1,13 +1,24 @@
 /* Write a Program to Flatten a given n-dimensional array */
 
-const flatten = () => {
-	// Write your code here
-};
-
-/* For example,
-INPUT - flatten([1, [2, 3], [[4], [5]])
-OUTPUT - [ 1, 2, 3, 4, 5 ]
-
-*/
-
-module.exports = flatten;
+const flatten = (a) => {
+    // Write your code here
+    var s;
+    if(typeof a == "object")
+    {
+    var c=[].concat(...a);
+    var d=[].concat(...c)
+    s=[].concat(...d);
+    }
+    else{
+    s=null;
+    }
+    return s;
+    };
+    
+    /* For example,
+    INPUT - flatten([1, [2, 3], [[4], [5]])
+    OUTPUT - [ 1, 2, 3, 4, 5 ]
+    
+    */
+    
+    module.exports = flatten;
