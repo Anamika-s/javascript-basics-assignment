127a0fc8cfe9c7e3dc432d006854e11b32731258
Anamika.Sawhney <anamika.9.sawhney@niit.com>
2021-08-23 00:19:05 +0530

    Update q1_pyramid_of_stars.js

--- solutions/q1_pyramid_of_stars.js
+++ solutions/q1_pyramid_of_stars.js
@@ -1,8 +1,25 @@
 /* Write a program to build a `Pyramid of stars` of given height */
 
-const buildPyramid = () => {
-	// Write my code here
+ 
+	const buildPyramid = (n) => {
+     // Write my code here
+     let s = "";
+     for(var i=1;i<=n;i++){
+     for(var j=n;j>=i;j--)
+     {
+        s +=" ";  
+     }
+     s +="*";
+     for(var k=2;k<=i;k++)
+     {
+          s +=" *";
+     }
+     s +="  \n";
+}
+return s;
 };
+
+ 
 
 /* For example,
 INPUT - buildPyramid(6)
