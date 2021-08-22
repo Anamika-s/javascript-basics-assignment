a8068578b0879dea6923a669f035d50de1d200cc
Anamika.Sawhney <anamika.9.sawhney@niit.com>
2021-08-23 00:19:45 +0530

    Update q2_array_to_object_converter.js

--- solutions/q2_array_to_object_converter.js
+++ solutions/q2_array_to_object_converter.js
@@ -1,19 +1,34 @@
 /* Write a Program to convert an array of objects to an object
-	based on a given key */
+    based on a given key */
 
 
-const convert = () => {
-	// Write your code here
-};
-
-/* For example,
-INPUT - convert([{id: 1, value: 'abc'}, {id: 2, value: 'xyz'}], 'id')
-OUTPUT - {
-			'1': {id: 1, value: 'abc'},
-			'2': {id: 2, value: 'xyz'}
-		 }
-
-
-*/
-
-module.exports = convert;
+	const convert = (array,x) => {
+		// Write your code here
+		var output={}
+		if(typeof array=="object")
+		{
+			for(let i = 0 ; i < array.length ;i++)
+			{
+				var value = array[i][x];
+				output[value] = array[i];
+			}
+			return output;
+		}
+		else
+		{
+			return null;
+		}
+	
+	};
+	
+	/* For example,
+	INPUT - convert([{id: 1, value: 'abc'}, {id: 2, value: 'xyz'}], 'id')
+	OUTPUT - {
+				'1': {id: 1, value: 'abc'},
+				'2': {id: 2, value: 'xyz'}
+			 }
+	
+	
+	*/
+	
+	module.exports = convert;
